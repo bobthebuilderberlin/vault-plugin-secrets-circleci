63da2f486387ceb621f705fd68354d56d52b8a1c
robert.andruschow <robert.andruschow@up42.com>
2022-06-05 09:22:17 +0200

    First tests for path_config_test.go

--- backend_test.go
+++ backend_test.go
@@ -2,28 +2,10 @@ package circleci
 
 import (
 	"context"
-	"errors"
-	"fmt"
-	"os"
-	"path"
-	"reflect"
+	hclog "github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/vault/sdk/logical"
 	"strings"
 	"testing"
-	"time"
-
-	"github.com/gammazero/workerpool"
-	"github.com/hashicorp/vault/sdk/helper/useragent"
-	"github.com/hashicorp/vault/sdk/logical"
-	"google.golang.org/api/iterator"
-	"google.golang.org/api/option"
-	"google.golang.org/grpc/connectivity"
-
-	kmsapi "cloud.google.com/go/kms/apiv1"
-	hclog "github.com/hashicorp/go-hclog"
-	uuid "github.com/satori/go.uuid"
-	kmspb "google.golang.org/genproto/googleapis/cloud/kms/v1"
-	grpccodes "google.golang.org/grpc/codes"
-	grpcstatus "google.golang.org/grpc/status"
 )
 
 // testBackend creates a new isolated instance of the backend for testing.
@@ -61,269 +43,269 @@ func testFieldValidation(tb testing.TB, op logical.Operation, pth string) {
 		tb.Error(err)
 	}
 }
-
-// testKMSClient creates a new KMS client with the default scopes and user
-// agent.
-func testKMSClient(tb testing.TB) *kmsapi.KeyManagementClient {
-	tb.Helper()
-
-	ctx := context.Background()
-	kmsClient, err := kmsapi.NewKeyManagementClient(ctx,
-		option.WithScopes(defaultScope),
-		option.WithUserAgent(useragent.String()),
-	)
-	if err != nil {
-		tb.Fatalf("failed to create kms client: %s", err)
-	}
-
-	return kmsClient
-}
-
-// testKMSKeyRingName creates a keyring name. If the given "name" is
-// blank, a UUID name is generated.
-func testKMSKeyRingName(tb testing.TB, name string) string {
-	tb.Helper()
-
-	project := os.Getenv("GOOGLE_CLOUD_PROJECT")
-	if project == "" {
-		tb.Fatal("missing GOOGLE_CLOUD_PROJECT")
-	}
-
-	if name == "" {
-		name = fmt.Sprintf("vault-test-%s", uuid.NewV4())
-	}
-
-	return fmt.Sprintf("projects/%s/locations/us-east1/keyRings/%s", project, name)
-}
-
-// testCreateKMSKeyRing creates a keyring with the given name.
-func testCreateKMSKeyRing(tb testing.TB, name string) (string, func()) {
-	tb.Helper()
-
-	keyRing := testKMSKeyRingName(tb, name)
-
-	kmsClient := testKMSClient(tb)
-
-	// Check if the key ring exists
-	ctx := context.Background()
-	kr, err := kmsClient.GetKeyRing(ctx, &kmspb.GetKeyRingRequest{
-		Name: keyRing,
-	})
-	if err != nil {
-		if terr, ok := grpcstatus.FromError(err); ok && terr.Code() == grpccodes.NotFound {
-			// Key ring does not exist, try to create it
-			kr, err = kmsClient.CreateKeyRing(ctx, &kmspb.CreateKeyRingRequest{
-				Parent:    path.Dir(path.Dir(keyRing)),
-				KeyRingId: path.Base(keyRing),
-			})
-			if err != nil {
-				tb.Fatalf("failed to create keyring: %s", err)
-			}
-		} else {
-			tb.Fatalf("failed to get keyring: %s", err)
-		}
-	}
-
-	return kr.Name, func() { testCleanupKeyRing(tb, kr.Name) }
-}
-
-// testCreateKMSCryptoKeySymmetric creates a new crypto key under the
-// vault-gcpkms-plugin-test key ring in the given google project.
-func testCreateKMSCryptoKeySymmetric(tb testing.TB) (string, func()) {
-	return testCreateKMSCryptoKeyPurpose(tb,
-		kmspb.CryptoKey_ENCRYPT_DECRYPT,
-		kmspb.CryptoKeyVersion_GOOGLE_SYMMETRIC_ENCRYPTION,
-	)
-}
-
-// testCreateKMSCryptoKeyAsymmetricDecrypt creates a new KMS crypto key that is
-// used for asymmetric decryption.
-func testCreateKMSCryptoKeyAsymmetricDecrypt(tb testing.TB, algo kmspb.CryptoKeyVersion_CryptoKeyVersionAlgorithm) (string, func()) {
-	return testCreateKMSCryptoKeyPurpose(tb,
-		kmspb.CryptoKey_ASYMMETRIC_DECRYPT,
-		algo,
-	)
-}
-
-// testCreateKMSCryptoKeyAsymmetricSign creates a new KMS crypto key that is
-// used for asymmetric signing.
-func testCreateKMSCryptoKeyAsymmetricSign(tb testing.TB, algo kmspb.CryptoKeyVersion_CryptoKeyVersionAlgorithm) (string, func()) {
-	return testCreateKMSCryptoKeyPurpose(tb,
-		kmspb.CryptoKey_ASYMMETRIC_SIGN,
-		algo,
-	)
-}
-
-
-
-func TestBackend_KMSClient(t *testing.T) {
-	t.Parallel()
-
-	t.Run("allows_concurrent_reads", func(t *testing.T) {
-		t.Parallel()
-
-		b, storage := testBackend(t)
-
-		_, closer1, err := b.CircleCIClient(storage)
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer closer1()
-
-		doneCh := make(chan struct{})
-		go func() {
-			_, closer2, err := b.CircleCIClient(storage)
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer closer2()
-			close(doneCh)
-		}()
-
-		select {
-		case <-doneCh:
-		case <-time.After(1 * time.Second):
-			t.Errorf("client was not available")
-		}
-	})
-
-	t.Run("caches", func(t *testing.T) {
-		t.Parallel()
-
-		b, storage := testBackend(t)
-
-		client1, closer1, err := b.CircleCIClient(storage)
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer closer1()
-
-		client2, closer2, err := b.CircleCIClient(storage)
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer closer2()
-
-		// Note: not a bug; literally checking object equality
-		if client1 != client2 {
-			t.Errorf("expected %#v to be %#v", client1, client2)
-		}
-	})
-
-	t.Run("expires", func(t *testing.T) {
-		t.Parallel()
-
-		b, storage := testBackend(t)
-		b.kmsClientLifetime = 50 * time.Millisecond
-
-		client1, closer1, err := b.CircleCIClient(storage)
-		if err != nil {
-			t.Fatal(err)
-		}
-		closer1()
-
-		time.Sleep(100 * time.Millisecond)
-
-		client2, closer2, err := b.CircleCIClient(storage)
-		if err != nil {
-			t.Fatal(err)
-		}
-		closer2()
-
-		if client1 == client2 {
-			t.Errorf("expected %#v to not be %#v", client1, client2)
-		}
-	})
-}
-
-func TestBackend_ResetClient(t *testing.T) {
-	t.Parallel()
-
-	t.Run("closes_client", func(t *testing.T) {
-		t.Parallel()
-
-		b, storage := testBackend(t)
-
-		client, closer, err := b.CircleCIClient(storage)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		// Verify the client is "open"
-		if client.Connection().GetState() == connectivity.Shutdown {
-			t.Fatalf("connection is already stopped")
-		}
-
-		// Stop read lock
-		closer()
-
-		// Reset the clients
-		b.ResetClient()
-
-		// Verify the client closed
-		if state := client.Connection().GetState(); state != connectivity.Shutdown {
-			t.Errorf("expected client to be closed, was: %v", state)
-		}
-	})
-}
-
-func TestBackend_Config(t *testing.T) {
-	t.Parallel()
-
-	cases := []struct {
-		name string
-		c    []byte
-		e    *Config
-		err  bool
-	}{
-		{
-			"default",
-			nil,
-			DefaultConfig(),
-			false,
-		},
-		{
-			"saved",
-			[]byte(`{"credentials":"foo", "scopes":["bar"]}`),
-			&Config{
-				Credentials: "foo",
-				Scopes:      []string{"bar"},
-			},
-			false,
-		},
-		{
-			"invalid",
-			[]byte(`{x`),
-			nil,
-			true,
-		},
-	}
-
-	for _, tc := range cases {
-		tc := tc
-
-		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
-
-			b, storage := testBackend(t)
-
-			if tc.c != nil {
-				if err := storage.Put(context.Background(), &logical.StorageEntry{
-					Key:   "config",
-					Value: tc.c,
-				}); err != nil {
-					t.Fatal(err)
-				}
-			}
-
-			c, err := b.Config(context.Background(), storage)
-			if (err != nil) != tc.err {
-				t.Fatal(err)
-			}
-
-			if !reflect.DeepEqual(c, tc.e) {
-				t.Errorf("expected %#v to be %#v", c, tc.e)
-			}
-		})
-	}
-}
+//
+//// testKMSClient creates a new KMS client with the default scopes and user
+//// agent.
+//func testKMSClient(tb testing.TB) *kmsapi.KeyManagementClient {
+//	tb.Helper()
+//
+//	ctx := context.Background()
+//	kmsClient, err := kmsapi.NewKeyManagementClient(ctx,
+//		option.WithScopes(defaultScope),
+//		option.WithUserAgent(useragent.String()),
+//	)
+//	if err != nil {
+//		tb.Fatalf("failed to create kms client: %s", err)
+//	}
+//
+//	return kmsClient
+//}
+//
+//// testKMSKeyRingName creates a keyring name. If the given "name" is
+//// blank, a UUID name is generated.
+//func testKMSKeyRingName(tb testing.TB, name string) string {
+//	tb.Helper()
+//
+//	project := os.Getenv("GOOGLE_CLOUD_PROJECT")
+//	if project == "" {
+//		tb.Fatal("missing GOOGLE_CLOUD_PROJECT")
+//	}
+//
+//	if name == "" {
+//		name = fmt.Sprintf("vault-test-%s", uuid.NewV4())
+//	}
+//
+//	return fmt.Sprintf("projects/%s/locations/us-east1/keyRings/%s", project, name)
+//}
+//
+//// testCreateKMSKeyRing creates a keyring with the given name.
+//func testCreateKMSKeyRing(tb testing.TB, name string) (string, func()) {
+//	tb.Helper()
+//
+//	keyRing := testKMSKeyRingName(tb, name)
+//
+//	kmsClient := testKMSClient(tb)
+//
+//	// Check if the key ring exists
+//	ctx := context.Background()
+//	kr, err := kmsClient.GetKeyRing(ctx, &kmspb.GetKeyRingRequest{
+//		Name: keyRing,
+//	})
+//	if err != nil {
+//		if terr, ok := grpcstatus.FromError(err); ok && terr.Code() == grpccodes.NotFound {
+//			// Key ring does not exist, try to create it
+//			kr, err = kmsClient.CreateKeyRing(ctx, &kmspb.CreateKeyRingRequest{
+//				Parent:    path.Dir(path.Dir(keyRing)),
+//				KeyRingId: path.Base(keyRing),
+//			})
+//			if err != nil {
+//				tb.Fatalf("failed to create keyring: %s", err)
+//			}
+//		} else {
+//			tb.Fatalf("failed to get keyring: %s", err)
+//		}
+//	}
+//
+//	return kr.Name, func() { testCleanupKeyRing(tb, kr.Name) }
+//}
+//
+//// testCreateKMSCryptoKeySymmetric creates a new crypto key under the
+//// vault-gcpkms-plugin-test key ring in the given google project.
+//func testCreateKMSCryptoKeySymmetric(tb testing.TB) (string, func()) {
+//	return testCreateKMSCryptoKeyPurpose(tb,
+//		kmspb.CryptoKey_ENCRYPT_DECRYPT,
+//		kmspb.CryptoKeyVersion_GOOGLE_SYMMETRIC_ENCRYPTION,
+//	)
+//}
+//
+//// testCreateKMSCryptoKeyAsymmetricDecrypt creates a new KMS crypto key that is
+//// used for asymmetric decryption.
+//func testCreateKMSCryptoKeyAsymmetricDecrypt(tb testing.TB, algo kmspb.CryptoKeyVersion_CryptoKeyVersionAlgorithm) (string, func()) {
+//	return testCreateKMSCryptoKeyPurpose(tb,
+//		kmspb.CryptoKey_ASYMMETRIC_DECRYPT,
+//		algo,
+//	)
+//}
+//
+//// testCreateKMSCryptoKeyAsymmetricSign creates a new KMS crypto key that is
+//// used for asymmetric signing.
+//func testCreateKMSCryptoKeyAsymmetricSign(tb testing.TB, algo kmspb.CryptoKeyVersion_CryptoKeyVersionAlgorithm) (string, func()) {
+//	return testCreateKMSCryptoKeyPurpose(tb,
+//		kmspb.CryptoKey_ASYMMETRIC_SIGN,
+//		algo,
+//	)
+//}
+//
+//
+//
+//func TestBackend_KMSClient(t *testing.T) {
+//	t.Parallel()
+//
+//	t.Run("allows_concurrent_reads", func(t *testing.T) {
+//		t.Parallel()
+//
+//		b, storage := testBackend(t)
+//
+//		_, closer1, err := b.CircleCIClient(storage)
+//		if err != nil {
+//			t.Fatal(err)
+//		}
+//		defer closer1()
+//
+//		doneCh := make(chan struct{})
+//		go func() {
+//			_, closer2, err := b.CircleCIClient(storage)
+//			if err != nil {
+//				t.Fatal(err)
+//			}
+//			defer closer2()
+//			close(doneCh)
+//		}()
+//
+//		select {
+//		case <-doneCh:
+//		case <-time.After(1 * time.Second):
+//			t.Errorf("client was not available")
+//		}
+//	})
+//
+//	t.Run("caches", func(t *testing.T) {
+//		t.Parallel()
+//
+//		b, storage := testBackend(t)
+//
+//		client1, closer1, err := b.CircleCIClient(storage)
+//		if err != nil {
+//			t.Fatal(err)
+//		}
+//		defer closer1()
+//
+//		client2, closer2, err := b.CircleCIClient(storage)
+//		if err != nil {
+//			t.Fatal(err)
+//		}
+//		defer closer2()
+//
+//		// Note: not a bug; literally checking object equality
+//		if client1 != client2 {
+//			t.Errorf("expected %#v to be %#v", client1, client2)
+//		}
+//	})
+//
+//	t.Run("expires", func(t *testing.T) {
+//		t.Parallel()
+//
+//		b, storage := testBackend(t)
+//		b.kmsClientLifetime = 50 * time.Millisecond
+//
+//		client1, closer1, err := b.CircleCIClient(storage)
+//		if err != nil {
+//			t.Fatal(err)
+//		}
+//		closer1()
+//
+//		time.Sleep(100 * time.Millisecond)
+//
+//		client2, closer2, err := b.CircleCIClient(storage)
+//		if err != nil {
+//			t.Fatal(err)
+//		}
+//		closer2()
+//
+//		if client1 == client2 {
+//			t.Errorf("expected %#v to not be %#v", client1, client2)
+//		}
+//	})
+//}
+//
+//func TestBackend_ResetClient(t *testing.T) {
+//	t.Parallel()
+//
+//	t.Run("closes_client", func(t *testing.T) {
+//		t.Parallel()
+//
+//		b, storage := testBackend(t)
+//
+//		client, closer, err := b.CircleCIClient(storage)
+//		if err != nil {
+//			t.Fatal(err)
+//		}
+//
+//		// Verify the client is "open"
+//		if client.Connection().GetState() == connectivity.Shutdown {
+//			t.Fatalf("connection is already stopped")
+//		}
+//
+//		// Stop read lock
+//		closer()
+//
+//		// Reset the clients
+//		b.ResetClient()
+//
+//		// Verify the client closed
+//		if state := client.Connection().GetState(); state != connectivity.Shutdown {
+//			t.Errorf("expected client to be closed, was: %v", state)
+//		}
+//	})
+//}
+//
+//func TestBackend_Config(t *testing.T) {
+//	t.Parallel()
+//
+//	cases := []struct {
+//		name string
+//		c    []byte
+//		e    *Config
+//		err  bool
+//	}{
+//		{
+//			"default",
+//			nil,
+//			DefaultConfig(),
+//			false,
+//		},
+//		{
+//			"saved",
+//			[]byte(`{"credentials":"foo", "scopes":["bar"]}`),
+//			&Config{
+//				Credentials: "foo",
+//				Scopes:      []string{"bar"},
+//			},
+//			false,
+//		},
+//		{
+//			"invalid",
+//			[]byte(`{x`),
+//			nil,
+//			true,
+//		},
+//	}
+//
+//	for _, tc := range cases {
+//		tc := tc
+//
+//		t.Run(tc.name, func(t *testing.T) {
+//			t.Parallel()
+//
+//			b, storage := testBackend(t)
+//
+//			if tc.c != nil {
+//				if err := storage.Put(context.Background(), &logical.StorageEntry{
+//					Key:   "config",
+//					Value: tc.c,
+//				}); err != nil {
+//					t.Fatal(err)
+//				}
+//			}
+//
+//			c, err := b.Config(context.Background(), storage)
+//			if (err != nil) != tc.err {
+//				t.Fatal(err)
+//			}
+//
+//			if !reflect.DeepEqual(c, tc.e) {
+//				t.Errorf("expected %#v to be %#v", c, tc.e)
+//			}
+//		})
+//	}
+//}
--- path_config_test.go
+++ path_config_test.go
@@ -1,218 +1,220 @@
 package circleci
-//
-//import (
-//	"context"
-//	"reflect"
-//	"testing"
-//
-//	"github.com/hashicorp/vault/sdk/logical"
-//)
-//
-//func TestBackend_PathConfigRead(t *testing.T) {
-//	t.Parallel()
-//
-//	t.Run("field_validation", func(t *testing.T) {
-//		t.Parallel()
-//		testFieldValidation(t, logical.ReadOperation, "config")
-//	})
-//
-//	t.Run("not_exist", func(t *testing.T) {
-//		t.Parallel()
-//
-//		b, storage := testBackend(t)
-//		ctx := context.Background()
-//		resp, err := b.HandleRequest(ctx, &logical.Request{
-//			Storage:   storage,
-//			Operation: logical.ReadOperation,
-//			Path:      "config",
-//		})
-//		if err != nil {
-//			t.Fatal(err)
-//		}
-//
-//		if _, ok := resp.Data["scopes"]; !ok {
-//			t.Errorf("expected %q to include %q", resp.Data, "scopes")
-//		}
-//	})
-//
-//	t.Run("exist", func(t *testing.T) {
-//		t.Parallel()
-//
-//		b, storage := testBackend(t)
-//
-//		entry, err := logical.StorageEntryJSON("config", &Config{
-//			Scopes:      []string{"foo"},
-//			Credentials: "creds",
-//		})
-//		if err != nil {
-//			t.Fatal(err)
-//		}
-//		if err := storage.Put(context.Background(), entry); err != nil {
-//			t.Fatal(err)
-//		}
-//
-//		ctx := context.Background()
-//		resp, err := b.HandleRequest(ctx, &logical.Request{
-//			Storage:   storage,
-//			Operation: logical.ReadOperation,
-//			Path:      "config",
-//		})
-//		if err != nil {
-//			t.Fatal(err)
-//		}
-//
-//		if v, exp := resp.Data["scopes"].([]string), []string{"foo"}; !reflect.DeepEqual(v, exp) {
-//			t.Errorf("expected %q to be %q", v, exp)
-//		}
-//
-//		if _, ok := resp.Data["credentials"]; ok {
-//			t.Errorf("should not return credentials")
-//		}
-//	})
-//}
-//
-//func TestBackend_PathConfigUpdate(t *testing.T) {
-//	t.Parallel()
-//
-//	t.Run("field_validation", func(t *testing.T) {
-//		t.Parallel()
-//		testFieldValidation(t, logical.UpdateOperation, "config")
-//	})
-//
-//	t.Run("not_exist", func(t *testing.T) {
-//		t.Parallel()
-//
-//		b, storage := testBackend(t)
-//		if _, err := b.HandleRequest(context.Background(), &logical.Request{
-//			Storage:   storage,
-//			Operation: logical.UpdateOperation,
-//			Path:      "config",
-//			Data: map[string]interface{}{
-//				"scopes":      "foo,bar",
-//				"credentials": "creds",
-//			},
-//		}); err != nil {
-//			t.Fatal(err)
-//		}
-//
-//		config, err := b.Config(context.Background(), storage)
-//		if err != nil {
-//			t.Fatal(err)
-//		}
-//
-//		if v, exp := config.Credentials, "creds"; v != exp {
-//			t.Errorf("expected %q to be %q", v, exp)
-//		}
-//
-//		if v, exp := config.Scopes, []string{"bar", "foo"}; !reflect.DeepEqual(v, exp) {
-//			t.Errorf("expected %q to be %q", v, exp)
-//		}
-//	})
-//
-//	t.Run("exist", func(t *testing.T) {
-//		t.Parallel()
-//
-//		b, storage := testBackend(t)
-//
-//		entry, err := logical.StorageEntryJSON("config", &Config{
-//			Scopes:      []string{"foo"},
-//			Credentials: "creds",
-//		})
-//		if err != nil {
-//			t.Fatal(err)
-//		}
-//		if err := storage.Put(context.Background(), entry); err != nil {
-//			t.Fatal(err)
-//		}
-//
-//		if _, err := b.HandleRequest(context.Background(), &logical.Request{
-//			Storage:   storage,
-//			Operation: logical.UpdateOperation,
-//			Path:      "config",
-//			Data: map[string]interface{}{
-//				"scopes":      "foo,bar",
-//				"credentials": "new-creds",
-//			},
-//		}); err != nil {
-//			t.Fatal(err)
-//		}
-//
-//		config, err := b.Config(context.Background(), storage)
-//		if err != nil {
-//			t.Fatal(err)
-//		}
-//
-//		if v, exp := config.Credentials, "new-creds"; v != exp {
-//			t.Errorf("expected %q to be %q", v, exp)
-//		}
-//
-//		if v, exp := config.Scopes, []string{"bar", "foo"}; !reflect.DeepEqual(v, exp) {
-//			t.Errorf("expected %q to be %q", v, exp)
-//		}
-//	})
-//}
-//
-//func TestBackend_PathConfigDelete(t *testing.T) {
-//	t.Parallel()
-//
-//	t.Run("field_validation", func(t *testing.T) {
-//		t.Parallel()
-//		testFieldValidation(t, logical.DeleteOperation, "config")
-//	})
-//
-//	t.Run("not_exist", func(t *testing.T) {
-//		t.Parallel()
-//
-//		b, storage := testBackend(t)
-//		if _, err := b.HandleRequest(context.Background(), &logical.Request{
-//			Storage:   storage,
-//			Operation: logical.DeleteOperation,
-//			Path:      "config",
-//		}); err != nil {
-//			t.Fatal(err)
-//		}
-//
-//		config, err := b.Config(context.Background(), storage)
-//		if err != nil {
-//			t.Fatal(err)
-//		}
-//
-//		if def := DefaultConfig(); !reflect.DeepEqual(config, def) {
-//			t.Errorf("expected %v to be %v", config, def)
-//		}
-//	})
-//
-//	t.Run("exist", func(t *testing.T) {
-//		t.Parallel()
-//
-//		b, storage := testBackend(t)
-//
-//		entry, err := logical.StorageEntryJSON("config", &Config{
-//			Scopes:      []string{"foo"},
-//			Credentials: "creds",
-//		})
-//		if err != nil {
-//			t.Fatal(err)
-//		}
-//		if err := storage.Put(context.Background(), entry); err != nil {
-//			t.Fatal(err)
-//		}
-//
-//		if _, err := b.HandleRequest(context.Background(), &logical.Request{
-//			Storage:   storage,
-//			Operation: logical.DeleteOperation,
-//			Path:      "config",
-//		}); err != nil {
-//			t.Fatal(err)
-//		}
-//
-//		config, err := b.Config(context.Background(), storage)
-//		if err != nil {
-//			t.Fatal(err)
-//		}
-//
-//		if def := DefaultConfig(); !reflect.DeepEqual(config, def) {
-//			t.Errorf("expected %v to be %v", config, def)
-//		}
-//	})
-//}
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/hashicorp/vault/sdk/logical"
+)
+
+func TestBackend_PathConfigRead(t *testing.T) {
+	t.Parallel()
+
+	t.Run("field_validation", func(t *testing.T) {
+		t.Parallel()
+		testFieldValidation(t, logical.ReadOperation, "config")
+	})
+
+	t.Run("not_exist", func(t *testing.T) {
+		t.Parallel()
+
+		b, storage := testBackend(t)
+		ctx := context.Background()
+		resp, err := b.HandleRequest(ctx, &logical.Request{
+			Storage:   storage,
+			Operation: logical.ReadOperation,
+			Path:      "config",
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if _, ok := resp.Data["APIToken"]; !ok {
+			t.Errorf("expected %q to include %q", resp.Data, "api-token")
+		}
+		if _, ok := resp.Data["OrgId"]; !ok {
+			t.Errorf("expected %q to include %q", resp.Data, "org-id")
+		}
+	})
+
+	t.Run("exist", func(t *testing.T) {
+		t.Parallel()
+
+		b, storage := testBackend(t)
+
+		entry, err := logical.StorageEntryJSON("config", &Config{
+			APIToken: "my-token",
+			OrgId:    "my-org-id",
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err := storage.Put(context.Background(), entry); err != nil {
+			t.Fatal(err)
+		}
+
+		ctx := context.Background()
+		resp, err := b.HandleRequest(ctx, &logical.Request{
+			Storage:   storage,
+			Operation: logical.ReadOperation,
+			Path:      "config",
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if resp.Data["APIToken"].(string) != "my-token" {
+			t.Errorf("expected api-token to be 'my-token'")
+		}
+		if resp.Data["OrgId"].(string) != "my-org-id" {
+			t.Errorf("expected org-id to be 'my-org-id'")
+		}
+	})
+}
+
+func TestBackend_PathConfigUpdate(t *testing.T) {
+	t.Parallel()
+
+	t.Run("field_validation", func(t *testing.T) {
+		t.Parallel()
+		testFieldValidation(t, logical.UpdateOperation, "config")
+	})
+
+	t.Run("not_exist", func(t *testing.T) {
+		t.Parallel()
+
+		b, storage := testBackend(t)
+		if _, err := b.HandleRequest(context.Background(), &logical.Request{
+			Storage:   storage,
+			Operation: logical.UpdateOperation,
+			Path:      "config",
+			Data: map[string]interface{}{
+				"api-token": "my-token",
+				"org-id":    "my-org-id",
+			},
+		}); err != nil {
+			t.Fatal(err)
+		}
+
+		config, err := b.Config(context.Background(), storage)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if v, exp := config.APIToken, "my-token"; v != exp {
+			t.Errorf("expected %q to be %q", v, exp)
+		}
+
+		if v, exp := config.OrgId, "my-org-id"; v != exp {
+			t.Errorf("expected %q to be %q", v, exp)
+		}
+	})
+
+	t.Run("exist", func(t *testing.T) {
+		t.Parallel()
+
+		b, storage := testBackend(t)
+
+		entry, err := logical.StorageEntryJSON("config", &Config{
+			APIToken: "my-token",
+			OrgId:    "my-org-id",
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err := storage.Put(context.Background(), entry); err != nil {
+			t.Fatal(err)
+		}
+
+		if _, err := b.HandleRequest(context.Background(), &logical.Request{
+			Storage:   storage,
+			Operation: logical.UpdateOperation,
+			Path:      "config",
+			Data: map[string]interface{}{
+				"api-token": "my-new-token",
+				"org-id":    "my-new-org-id",
+			},
+		}); err != nil {
+			t.Fatal(err)
+		}
+
+		config, err := b.Config(context.Background(), storage)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if v, exp := config.APIToken, "my-new-token"; v != exp {
+			t.Errorf("expected %q to be %q", v, exp)
+		}
+
+		if v, exp := config.OrgId, "my-new-org-id"; v != exp {
+			t.Errorf("expected %q to be %q", v, exp)
+		}
+	})
+}
+
+func TestBackend_PathConfigDelete(t *testing.T) {
+	t.Parallel()
+
+	t.Run("field_validation", func(t *testing.T) {
+		t.Parallel()
+		testFieldValidation(t, logical.DeleteOperation, "config")
+	})
+
+	t.Run("not_exist", func(t *testing.T) {
+		t.Parallel()
+
+		b, storage := testBackend(t)
+		if _, err := b.HandleRequest(context.Background(), &logical.Request{
+			Storage:   storage,
+			Operation: logical.DeleteOperation,
+			Path:      "config",
+		}); err != nil {
+			t.Fatal(err)
+		}
+
+		config, err := b.Config(context.Background(), storage)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if def := DefaultConfig(); !reflect.DeepEqual(config, def) {
+			t.Errorf("expected %v to be %v", config, def)
+		}
+	})
+
+	t.Run("exist", func(t *testing.T) {
+		t.Parallel()
+
+		b, storage := testBackend(t)
+
+		entry, err := logical.StorageEntryJSON("config", &Config{
+			APIToken: "my-token",
+			OrgId:    "my-org-id",
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err := storage.Put(context.Background(), entry); err != nil {
+			t.Fatal(err)
+		}
+
+		if _, err := b.HandleRequest(context.Background(), &logical.Request{
+			Storage:   storage,
+			Operation: logical.DeleteOperation,
+			Path:      "config",
+		}); err != nil {
+			t.Fatal(err)
+		}
+
+		config, err := b.Config(context.Background(), storage)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if def := DefaultConfig(); !reflect.DeepEqual(config, def) {
+			t.Errorf("expected %v to be %v", config, def)
+		}
+	})
+}
